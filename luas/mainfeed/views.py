fd063000ed413f29a71593fa8f6a712f245d95a1
amar <amartya.sk2@gmail.com>
2019-02-06 21:15:04 +0700

    fully removed schedule

--- luas/mainfeed/views.py
+++ luas/mainfeed/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
-import schedule
 import time
 from .models import Headline
 from .scrape import scrape
